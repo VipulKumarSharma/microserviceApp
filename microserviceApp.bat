e4ed10c44f2093058165f5672655c6dbbc0e5625
VipulKumarSharma <tysonvks@gmail.com>
2018-04-02 20:05:24 +0530

    Added commands to create Jhipster microservice application

---
+++ microserviceApp.bat
@@ -0,0 +1,12 @@
+# Jhipster Microservice Application ::
+
+# Install Yarn using installer, then execute below commands :
+> yarn global add yo
+> yarn global add generator-jhipster
+
+# You can also install Jhipster using "npm"
+> npm install -g yo
+> npm install -g generator-jhipster
+
+# Open cmd or powershell in a specific folder & execute below command to create jhipster application :
+> yo jhipster
